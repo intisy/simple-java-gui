06474bcaf383f55fcc0889487339e8ccc89f8569
finn <wilderpizza99@gmail.com>
2024-10-23 17:55:54 +0200

    Update BasicButton.java

--- src/main/java/io/github/intisy/gui/javafx/BasicButton.java
+++ src/main/java/io/github/intisy/gui/javafx/BasicButton.java
@@ -9,6 +9,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.lang.Double;
+
 @SuppressWarnings("unused")
 public class BasicButton extends ButtonBase {
     double height;
@@ -18,7 +20,7 @@ public class BasicButton extends ButtonBase {
     private Color selectedBackgroundColor;
     private boolean selected;
     private Label label;
-    private Double fontHeight;
+    private java.lang.Double fontHeight;
     private Double fontWidth;
     public BasicButton(String text) {
         this(text, 100, 30);
@@ -28,6 +30,8 @@ public class BasicButton extends ButtonBase {
     }
     public BasicButton(String text, double width, double height) {
         super(width, height);
+        setHeight(height);
+        setWidth(width);
         this.height = height;
         this.width = width;
         this.textFillColor = Colors.textColor;
@@ -39,18 +43,12 @@ public class BasicButton extends ButtonBase {
         if (text != null) {
             this.label = new Label(text);
             this.label.setTextFill(textFillColor);
-            label.widthProperty().addListener((observable, oldValue, newValue) -> {
-                Font font = this.label.getFont();
-                double size = font.getSize() * width / newValue.doubleValue() * 0.8;
-                fontWidth = size;
-                setFontSize(size);
-            });
-            label.heightProperty().addListener((observable, oldValue, newValue) -> {
-                Font font = this.label.getFont();
-                double size = font.getSize() * height / newValue.doubleValue() * 0.8;
-                fontHeight = size;
-                setFontSize(size);
-            });
+            Font font = new Font(this.label.getFont().getFamily(), this.label.getFont().getSize()*(Math.min(height, width)/22));
+            this.label.setFont(font);
+            Text fontText = new Text(text);
+            fontText.setFont(font);
+            this.label.setLayoutY((height - fontText.getBoundsInLocal().getHeight()) /2);
+            this.label.setLayoutX((width - fontText.getBoundsInLocal().getWidth()) /2);
             getChildren().add(this.label);
         }
         setOnMouseClicked(event -> {
@@ -100,6 +98,10 @@ public class BasicButton extends ButtonBase {
         this.selected = false;
     }
 
+    public final void setXOffset(double xOffset) {
+        this.label.setLayoutX(xOffset);
+    }
+
     public final void setOnAction(EventHandler<ActionEvent> var1) {
         this.onAction.set(var1);
     }
@@ -107,23 +109,10 @@ public class BasicButton extends ButtonBase {
         return this.label.getFont();
     }
     public final void setFont(Font font) {
-        if (fontHeight != null && fontWidth != null) {
-            Text fontText = new Text(this.label.getText());
-            fontText.setFont(this.label.getFont());
-            double fontSize = Math.min(fontHeight, fontWidth);
-            this.label.setLayoutY((this.height - fontSize * height / font.getSize()) / 2);
-            this.label.setLayoutX((this.width - fontSize * width / font.getSize()) / 2);
-            this.label.setFont(font);
-            System.out.println(fontHeight);
-            System.out.println(fontWidth);
-            System.out.println(fontSize);
-            System.out.println(fontSize * height / font.getSize());
-            System.out.println(fontSize * width / font.getSize());
-            fontWidth = null;
-            fontHeight = null;
-        }
-    }
-    public final void setFontSize(double size) {
-        setFont(new Font(this.label.getFont().getFamily(), size));
+        this.label.setFont(font);
+        Text fontText = new Text(this.label.getText());
+        fontText.setFont(this.label.getFont());
+        this.label.setLayoutY((this.height - fontText.getBoundsInLocal().getHeight()) /2);
+        this.label.setLayoutX((this.width - fontText.getBoundsInLocal().getWidth()) /2);
     }
 }
